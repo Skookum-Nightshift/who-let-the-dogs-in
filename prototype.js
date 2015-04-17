113b2c233ea731a2b105073220e1aa3b0662a44c
Mark Pedrotti <mark@marks-mbp.gateway.2wire.net>
2015-04-17 19:41:58 -0400

    most CSS style moves to JSX style attributes

--- prototype.js
+++ prototype.js
@@ -2,11 +2,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
 var DogsApp = React.createClass({
         propTypes: {
+            colors: React.PropTypes.object,
+            layout: React.PropTypes.object,
             categories: React.PropTypes.array,
             items: React.PropTypes.object
         },
         getDefaultProps: function () {
             return {
+                colors: {
+                    colorMeta: '#0a5a83', // blue
+                    colorItem: '#000000', // black
+                    colorBackground: '#ffffff' // white
+                },
+                layout: {
+                    lineHeightMeta: '2.5rem',
+                    widthCategorySymbol: '2rem',
+                    padding: '0.5rem'
+                },
                 categories: [{ 
                     key: 'bars',
                     text: 'Bars',
@@ -45,7 +57,7 @@ var DogsApp = React.createClass({
             } else {
                 // TO DO: LocationPage?
                 return (
-                    <ResultPage categories={this.props.categories} items={this.props.items} idsOrdered={this.state.idsOrdered}/>
+                    <ResultPage colors={this.props.colors} layout={this.props.layout} categories={this.props.categories} items={this.props.items} idsOrdered={this.state.idsOrdered} />
                 );
             }
         }
@@ -53,13 +65,18 @@ var DogsApp = React.createClass({
 
     ResultPage = React.createClass({
         propTypes: {
+            colors: React.PropTypes.object,
+            layout: React.PropTypes.object,
             categories: React.PropTypes.array,
             items: React.PropTypes.object,
             idsOrdered: React.PropTypes.array
         },
         getInitialState: function () {
             var items = this.props.items,
-                categoriesSelected = {};
+                categoriesSelected = {},
+                props = this.props,
+                layout = props.layout,
+                colors = props.colors;
 
             this.props.categories.forEach(function (category) {
                 categoriesSelected[category.key] = false; // all false means unfiltered
@@ -68,7 +85,17 @@ var DogsApp = React.createClass({
             return {
                 initial: true,
                 categoriesSelected: categoriesSelected,
-                idsFiltered: this.props.idsOrdered.concat() // copy
+                idsFiltered: props.idsOrdered.concat(), // copy
+                styleHeader: {
+                    //dispay: 'flex',
+                    lineHeight: layout.lineHeightMeta,
+                    paddingLeft: layout.widthCategorySymbol,
+                    paddingRight: layout.padding,
+                    color: colors.colorMeta,
+                    backgroundColor: colors.colorBackground,
+                    borderWidth: '3px',
+                    borderBottomStyle: 'solid'
+                }
             };
         },
         onCategorySelected: function (category) {
@@ -78,9 +105,7 @@ var DogsApp = React.createClass({
                 items = this.props.items,
                 idsFiltered = [];
 
-console.dir(categoriesSelected);
             categoriesSelected[key] = !categoriesSelected[key];
-console.dir(categoriesSelected);
             this.props.categories.forEach(function (category) {
                 noneSelected = noneSelected && !categoriesSelected[category.key];
             });
@@ -98,14 +123,17 @@ console.dir(categoriesSelected);
             });
         },
         render: function () {
+            var props = this.props,
+                state = this.state;
+
             // TODO: Header class?
             return (
                 <div>
-                    <header>
+                    <header style={this.state.styleHeader}>
                         <h1>Dogs-in</h1>
                     </header>
-                    <CategoryList categories={this.props.categories} categoriesSelected={this.state.categoriesSelected} onCategorySelected={this.onCategorySelected} />
-                    <ResultList items={this.props.items} idsFiltered={this.state.idsFiltered} />
+                    <CategoryList colors={props.colors} layout={props.layout} categories={props.categories} categoriesSelected={state.categoriesSelected} onCategorySelected={this.onCategorySelected} />
+                    <ResultList items={props.items} idsFiltered={state.idsFiltered} layout={props.layout} />
                 </div>
             );
         }
@@ -113,40 +141,77 @@ console.dir(categoriesSelected);
 
     CategoryList = React.createClass({
         propTypes: {
+            colors: React.PropTypes.object,
+            layout: React.PropTypes.object,
             categories: React.PropTypes.array,
             categoriesSelected: React.PropTypes.object,
             onCategorySelected: React.PropTypes.func
         },
+        style: {
+            listStyle: 'none',
+        },
         render: function () {
-            var categoriesSelected = this.props.categoriesSelected,
-                onCategorySelected = this.props.onCategorySelected,
-                categoryItems = this.props.categories.map(function (category) {
-                    return <CategoryItem category={category} selected={categoriesSelected[category.key]} onCategorySelected={onCategorySelected} />;
+            var props = this.props,
+                categoriesSelected = props.categoriesSelected,
+                onCategorySelected = props.onCategorySelected,
+                colors = props.colors,
+                layout = props.layout,
+                categoryItems = props.categories.map(function (category) {
+                    return <CategoryItem colors={colors} layout={layout} category={category} selected={categoriesSelected[category.key]} onCategorySelected={onCategorySelected} />;
                 });
 
             return (
-                <ul id='categories'>{categoryItems}</ul>
+                <ul id='categories' style={this.style}>{categoryItems}</ul>
             );
         }
     }),
 
     CategoryItem = React.createClass({
         propTypes: {
+            colors: React.PropTypes.object,
+            layout: React.PropTypes.object,
             category: React.PropTypes.object,
             selected: React.PropTypes.bool,
             onCategorySelected: React.PropTypes.func
+        },
+        getInitialState: function () {
+            var props = this.props,
+                selected = props.selected,
+                colors = props.colors,
+                colorMeta = colors.colorMeta,
+                colorBackground = colors.colorBackground,
+                layout = props.layout;
+
+            return {
+                styleItem: {
+                    display: 'flex',
+                    alignItems: 'flex-start',
+                    lineHeight: layout.lineHeightMeta,
+                    color: selected ? colorBackground : colorMeta,
+                    backgroundColor: selected ? colorMeta : colorBackground,
+                    borderWidth: '1px',
+                    borderBottomStyle: 'solid'
+                },
+                styleSymbol: {
+                    flexShrink: 0,
+                    width: layout.widthCategorySymbol
+                },
+                styleText: {
+                    flexShrink: 1
+                }
+            };
         },
         onClick: function () {
             this.props.onCategorySelected(this.props.category);
         },
         render: function () {
-            var category = this.props.category,
-                classSymbol = 'fa fa-' + category.symbol;
+            var state = this.state;
 
+            // TODO: SVG for category symbol
             return (
-                <li aria-selected={this.props.selected} onClick={this.onClick}>
-                    <span className='symbol'>{'\uf000'}</span>
-                    <span className='text'>{category.text}</span>
+                <li style={state.styleItem} aria-selected={this.props.selected} onClick={this.onClick}>
+                    <span style={state.styleSymbol} className='symbol'></span>
+                    <span style={state.styleText} className='text'>{this.props.category.text}</span>
                 </li>
             );
         }
@@ -155,38 +220,72 @@ console.dir(categoriesSelected);
     ResultList = React.createClass({
         propTypes: {
             items: React.PropTypes.object,
-            idsFiltered: React.PropTypes.array
+            idsFiltered: React.PropTypes.array,
+            layout: React.PropTypes.object
+        },
+        style: {
+            listStyle: 'none'
         },
         render: function () {
-            var items = this.props.items,
-                resultItems = this.props.idsFiltered.map(function (id) {
+            var props = this.props,
+                items = props.items,
+                layout = props.layout,
+                resultItems = props.idsFiltered.map(function (id) {
                         return (
-                            <ResultItem item={items[id]} />
+                            <ResultItem item={items[id]} layout={layout} />
                         );
                     });
 
             return (
-                <ul id='list'>{resultItems}</ul>
+                <ul id='list' style={this.style}>{resultItems}</ul>
             );
         },
     }),
 
     ResultItem = React.createClass({
         propTypes: {
-            item: React.PropTypes.object
+            item: React.PropTypes.object,
+            layout: React.PropTypes.object
+        },
+        getInitialState: function () {
+            var layout = this.props.layout;
+
+            return {
+                styleItem: {
+                    display: 'flex',
+                    alignItems: 'flex-start',
+                    paddingTop: layout.padding,
+                    paddingBottom: layout.padding,
+                    borderWidth: '1px',
+                    borderBottomStyle: 'dotted'
+                },
+                styleCategory: {
+                    flexShrink: 0,
+                    width: layout.widthCategorySymbol
+                },
+                styleDiv: {
+                    flexShrink: 1
+                },
+                styleDistance: {
+                    flexShrink: 0,
+                    marginLeft: 'auto', // align right
+                    marginRight: layout.padding
+                }
+            }
         },
         render: function () {
             var item = this.props.item,
-                distance = item.distance + 'mi';
+                distance = item.distance + 'mi',
+                state = this.state;
 
             return (
-                <li>
-                    <span className='category'>{'\uf000'}</span>
-                    <div>
-                        <span className='name'>{item.name}</span>
-                        <span className='address'>{item.address}</span>
+                <li style={state.styleItem}>
+                    <span className='category' style={state.styleCategory}></span>
+                    <div style={state.styleDiv}>
+                        <p className='name'>{item.name}</p>
+                        <p className='address'>{item.address}</p>
                     </div>
-                    <span className='distance'>{distance}</span>
+                    <span className='distance' style={state.styleDistance}>{distance}</span>
                 </li>
             );
         }
