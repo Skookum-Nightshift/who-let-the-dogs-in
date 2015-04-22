709208c2203ce6dc281151ffce5c9f4cce17465c
Mark Pedrotti <mark@marks-mbp.gateway.2wire.net>
2015-04-22 16:07:21 -0400

    change from jsx-loader to babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.jsx$/, loader: 'jsx-loader' }
+//            { test: /\.jsx$/, loader: 'jsx-loader' }
+            { test: /\.jsx$/, loader: 'babel-loader?{"stage": 0, "optional": "runtime"}'}
         ]
     }
 };
